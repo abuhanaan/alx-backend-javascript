35934436af4896585ee5dfa12522f8d175a675fb
abuhanaan <adewunmi99@gmail.com>
2022-04-13 04:08:02 +0100

    Cleaning up

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,4 @@
 export default function getStudentsByLocation(students, city) {
-    const result = students.filter(student => student.location === city);
-    return result;
+  const result = students.filter(student => student.location === city);
+  return result;
 }
--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,6 @@
 export default function getStudentIdsSum(students) {
-    const initialVal = 0;
-    const sum = students.reduce(
-        (prevStudent, currStudent) => prevStudent + currStudent.id, initialVal);
-    return sum;
+  const initialVal = 0;
+  const sum = students.reduce(
+    (prevStudent, currStudent) => prevStudent + currStudent.id, initialVal);
+  return sum;
 }
